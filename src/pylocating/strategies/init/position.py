13440aebe23c598a7d7a4530093e256877181b7f
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2015-10-17 13:05:37 +0200

    strategies: position init additions
    * Adds new strategy to initialize particle position around beacons.
    
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

---
+++ src/pylocating/strategies/init/position.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of pysenslog.
+# Copyright 2015 Leonardo Rossi <leonardo.rossi@studenti.unipr.it>.
+#
+# pysenslog is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# pysenslog is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with pysenslog.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Init Strategy: position."""
+
+from __future__ import absolute_import, unicode_literals
+
+
+def around_beacons(environment):
+    """Generate position around the beacons."""
+    i = 0
+    while True:
+        random = environment.config['random']
+        base = environment.config['base']
+        (row, column) = base.shape
+        yield base[i] + random.random()
+        i = (i + 1)%row
