033bb0c4a0b7951f76bd022f82b9146c4079d155
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2015-10-27 16:00:17 +0100

    benchmarks: moving point engine addition
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

---
+++ src/pylocating/benchmarks/moving_point.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of pysenslog.
+# Copyright 2015 Leonardo Rossi <leonardo.rossi@studenti.unipr.it>.
+#
+# pysenslog is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# pysenslog is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with pysenslog.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Moving point."""
+
+from __future__ import absolute_import, unicode_literals
+
+import logging
+
+from numpy import matrix
+from threading import Event, Thread
+from time import sleep
+
+
+class MovingPointEngine(Thread):
+
+    """Animate a point: follow a defined path."""
+
+    def __init__(self, config, listener, positions, stop_condition=None):
+        """Init moving particle.
+
+        :param config: dict container for configuration
+        :param point: point to move
+        :param positions: next positions
+        :param stop_condition: necessary condition to end the computation
+        """
+        super(MovingPointEngine, self).__init__()
+        self.config = config
+        self.shutdown_event = Event()
+        self.listener = listener
+        self.positions = positions
+        self.stop_condition = stop_condition or \
+            (lambda mp: mp.iterations >= 100)
+        self.logger = logging.getLogger(self.__class__.__module__ +
+                                        "." + self.__class__.__name__)
+
+    def run(self):
+        """The main method for the thread.
+
+        Called by the ``start()`` method.  Not to be called directly.
+        """
+        self.iterations = 0
+        while not self.shutdown_event.is_set() and \
+                not self.stop_condition(self):
+            # generate next position
+            try:
+                point = next(self.positions)
+                # log nex position
+                self.logger.debug("{} {}".format(self.iterations, point))
+                # alert listener
+                self.listener.position(point)
+                # sleep
+                sleep(self.config['sleep'])
+                # update counter
+                self.iterations = self.iterations + 1
+            except StopIteration:
+                self.shutdown()
+
+    def shutdown(self):
+        """Shutdown the engine."""
+        self.shutdown_event.set()
---
+++ tests/test_benchmarks_moving_point.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of pysenslog.
+# Copyright 2015 Leonardo Rossi <leonardo.rossi@studenti.unipr.it>.
+#
+# pysenslog is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# pysenslog is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with pysenslog.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Test Benchmarks Moving Point Engine."""
+
+from __future__ import absolute_import, unicode_literals
+
+from numpy import matrix
+from pylocating.benchmarks.moving_point import MovingPointEngine
+
+
+class TestBenchmarksMatrixGenerator(object):
+
+    """Test Benchmarks Matrix generator."""
+
+    def test_generate_matrix_of_points_in_cube(self):
+        """Test moving point engine."""
+        config = {'sleep': 0.01}
+        point = matrix([0, 0, 0])
+
+        def generator():
+            while True:
+                yield point
+
+        class Listener(object):
+            def position(self, value):
+                pass
+
+        # test 1
+        count = 5
+        mp = MovingPointEngine(config=config, listener=Listener(),
+                         positions=generator(),
+                         stop_condition=lambda mp: mp.iterations >= count)
+        mp.start()
+        mp.join()
+        assert mp.iterations == count
+
+        # test 2
+        count = 43
+        mp = MovingPointEngine(config=config, listener=Listener(),
+                         positions=generator(),
+                         stop_condition=lambda mp: mp.iterations >= count)
+        mp.start()
+        mp.join()
+        assert mp.iterations == count
+
+        # test 3
+        count = -1
+        mp = MovingPointEngine(config=config, listener=Listener(),
+                         positions=generator(),
+                         stop_condition=lambda mp: mp.iterations >= count)
+        mp.start()
+        mp.join()
+        assert mp.iterations == 0
+
+    def test_default_generator(self):
+        """Test default generator."""
+        config = {'sleep': 0.01}
+        point = matrix([0, 0, 0])
+
+        def generator():
+            i = point
+            while True:
+                yield i
+                i = i + 1
+
+        class Listener(object):
+
+            def __init__(self):
+                self._point = point
+
+            def position(self, value):
+                assert (self._point == value).all()
+                self._point = self._point + 1
+
+        # test 1
+        count = 12
+        mp = MovingPointEngine(config=config, listener=Listener(),
+                         positions=generator(),
+                         stop_condition=lambda mp: mp.iterations >= count)
+        mp.start()
+        mp.join()
+        assert mp.iterations == count
+
+    def test_stop_iteration(self):
+        """Test when finish next positions."""
+        config = {'sleep': 0.01}
+        point = matrix([0, 0, 0])
+
+        def generator():
+            return iter([
+                matrix([0, 0, 0]), matrix([1, 1, 1]), matrix([2, 2, 2]),
+                matrix([3, 3, 3])
+            ])
+
+        class Listener(object):
+
+            def __init__(self):
+                self._point = point
+
+            def position(self, value):
+                assert (self._point == value).all()
+                self._point = self._point + 1
+
+        # test 1
+        count = 12
+        mp = MovingPointEngine(config=config, listener=Listener(),
+                         positions=generator(),
+                         stop_condition=lambda mp: mp.iterations >= count)
+        mp.start()
+        mp.join()
+        assert mp.iterations == 4
